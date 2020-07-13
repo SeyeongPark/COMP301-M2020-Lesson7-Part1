c661f22ab02882766c514be224e4455cc7213b82
SeyeongPark <lynn.yeong.102@gmail.com>
2020-07-13 12:41:40 -0400

    test loops and conditionals

--- first.py
+++ first.py
@@ -1,5 +1,36 @@
-a, b = 5,10
+
+#this is a comment!
+a, b, c = 2,10, None # two variables
+A, B, C =2,30,40
 
 print("Hello, World!")
 
 print(f"hello a: {a} b: {b}")
+
+if (a<b):
+    print("A is less than b")
+elif(a>b):
+    print("A is not greater than b")    
+else:
+    print("A is equal to B")
+
+if(a == A):
+    print("a is equal to A")
+else:
+    print("a is not equal to A")
+
+
+for i in range(1, 6):
+    print(i)
+
+count=0 #sential variable
+
+while count < 10:
+    print(count)
+    count+=1
+
+myArray=[1,3,5,7,9]
+
+for elment in myArray:
+    print(elment)
+
