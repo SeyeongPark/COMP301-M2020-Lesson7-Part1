5d08ae0daa6c5a4978a883ea293341afb8b49b81
SeyeongPark <lynn.yeong.102@gmail.com>
2020-07-18 12:22:21 -0400

    added Distance utility function to Vector2D

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
 from student import Person
+from vector2D import Vector2D
 
 
-sey = Student("Sey", 21, "301088175")
+# Student("Sey", 21, "301088175")
 
-sey.saysHello()
-sey.studies()
+#sey.saysHello()
+#sey.studies()
+ 
+position1 = Vector2D(20.0, 30.1)
+position2 = Vector2D(10.0, 10.10)
+position3 = Vector2D(100.3, 87.4)
+
+position3 = position1 + position2 
+
+print(position1)
+
+position4 = position3 - position2
+print(position4)
--- vector2D.py
+++ vector2D.py
@@ -4,9 +4,30 @@ class Vector2D(object):
         self.x=x
         self.y=y
 
+    #overload the binary
+    
     def __add__(self, lhs, rhs):
         Xs = self.x + rhs.x
         Ys = self.y + rhs.y
 
         self.x = Xs
         self.y = Ys
+
+        return self.x, self.y
+        
+    #overload the -
+    def __sub__(self, rhs):
+        diffXs = self.x - rhs.x
+        diffYs = self.y - rhs.y
+
+        self.x = diffXs
+        self.y = diffYs
+
+        return self.x, self.y
+
+        #utility funtion for distance
+        def Distance(vector1, vector2):
+            diffXs = vector2.x - vector1.x
+            diffYs = vector2.y - vector1.y
+            return (diffXs * diffXs + diffYs * diffYs)
+
