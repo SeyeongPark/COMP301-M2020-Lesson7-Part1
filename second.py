efcfd5c9d95d0d42f82b44ec5973dba669e13b70
SeyeongPark <lynn.yeong.102@gmail.com>
2020-07-18 07:17:34 -0400

    enter

---
+++ second.py
@@ -0,0 +1,30 @@
+a=10
+b=20
+c= a+b
+
+e=a**3
+
+print(f"{a}+{b}={c}", end="\n")
+print("is a great resualt!")
+
+print(e)
+
+floor = 5//2
+print(floor)
+
+remainder = 5%2
+print(remainder)
+
+print("a is equal to : "+str(a))
+
+#print("Enter your name: ", end="")
+name = input("Enter your name: ")
+print("You type: " +name)
+
+xString = input("Enter the x value:  ")
+x=float (xString)
+
+yString = input("Enter the y value:  ")
+y=float (yString)
+
+print(f"({x}, {y})")
